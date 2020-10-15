5dd46d474970f898c98356a89ecaf3324d0a68e1
netanelxa <58979197+netanelxa@users.noreply.github.com>
2020-10-15 22:20:05 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,16 @@
+$("#add_new").click(function () {
+
+    $("#maintable").each(function () {
+
+        var tds = '<tr>';
+        jQuery.each($('tr:last td', this), function () {
+            tds += '<td>' + $(this).html() + '</td>';
+        });
+        tds += '</tr>';
+        if ($('tbody', this).length > 0) {
+            $('tbody', this).append(tds);
+        } else {
+            $(this).append(tds);
+        }
+    });
+});
